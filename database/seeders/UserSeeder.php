5508c1262c8a365e57d8911b653f51473a8cbb11
Muhammad Luthfi Hakim <146607038+muhfihakim@users.noreply.github.com>
2026-07-16 20:19:16 +0700

    fix: restore tenant creation

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,12 +12,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $tenant = \App\Models\Tenant::firstOrCreate(['id' => 'majumundur']);
+        
         $admin = \App\Models\User::firstOrCreate([
             'email' => 'admin@majumundur.com'
         ], [
             'name' => 'Admin Maju Mundur',
             'password' => bcrypt('password123'),
-            'tenant_id' => 'majumundur', // just set string without creating tenant db
+            'tenant_id' => $tenant->id,
         ]);
 
         $admin->assignRole('Admin');
